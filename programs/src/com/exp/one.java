e28a7650d7d72c54a8479632197df17cc6c3b47b
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-06 15:40:16 +0530

    list

---
+++ programs/src/com/exp/one.java
@@ -0,0 +1,7 @@
+package com.exp;
+
+public class one {
+    static void main() {
+        System.out.println("Hello");
+    }
+}
---
+++ programs/src/com/exp/two.java
@@ -0,0 +1,7 @@
+package com.exp;
+
+public class two {
+    public static void main(String[] args) {
+        System.out.println("Name: " + args[0]);
+    }
+}
