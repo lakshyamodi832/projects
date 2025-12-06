9f78c9eba10ae6573e778587febba5b8b28316bc
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-06 16:45:20 +0530

    three

--- programs/src/com/exp/three/Car.java
+++ programs/src/com/exp/three/Car.java
@@ -1,4 +1,23 @@
 package com.exp.three;
 
 public class Car {
+    String make;
+    String color;
+
+    public Car(String color, String make){
+        this.make = make;
+        this.color = color;
+    }
+
+    public String start(){
+        return "The " + color + " " + make + " car is starting.";
+    }
+
+    static void main() {
+        Car car1 = new Car("Red", "Skoda");
+        Car car2 = new Car("White", "Toyota");
+
+        System.out.println(car1.start());
+        System.out.println(car2.start());
+    }
 }
