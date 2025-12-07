11760dbcf858136530bc1d54d84dcc46d321be9d
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-07 10:40:36 +0530

    sBuffer

---
+++ programs/src/com/exp/four/sBuffer.java
@@ -0,0 +1,10 @@
+package com.exp.four;
+
+public class sBuffer {
+    static void main() {
+        StringBuffer sb = new StringBuffer("Hello");
+        System.out.println(sb.append(" World"));
+        System.out.println(sb.insert(0, "Hi, "));
+        System.out.println(sb.reverse());
+    }
+}
