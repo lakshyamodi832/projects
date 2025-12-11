d07199a8d7388ccbd9c20f1962829e82ebf8f81e
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-11 14:54:08 +0530

    stat

--- programs/src/com/exp/six/statKey.java
+++ programs/src/com/exp/six/statKey.java
@@ -2,8 +2,26 @@ package com.exp.six;
 
 public class statKey {
     static void main() {
-        String name = "Lakshya";
+        stats s1 = new stats(69);
 
+        s1.display();
+    }
+}
 
+class stats{
+    int exp;
+    static int stat = 12;
+
+    stats(){
+
+    }
+
+    stats(int exp){
+        this.exp = exp;
+    }
+
+    void display(){
+        System.out.println(exp);
+        System.out.println(stat);
     }
 }
