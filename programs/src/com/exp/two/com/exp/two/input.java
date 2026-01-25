9d4b4c33e28340f18a3f06d7bd257c7ee40ad75d
lakshyamodi832 <lakshyaymodi@gmail.com>
2026-01-25 19:26:48 +0530

    two

---
+++ programs/src/com/exp/two/com/exp/two/input.java
@@ -0,0 +1,13 @@
+package com.exp.two;
+
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Name: ");
+        String s = sc.nextLine();
+        System.out.println("Name: " + s);
+        sc.close();
+    }
+}
--- programs/src/com/exp/two/two.java
+++
@@ -1,8 +0,0 @@
-class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Name: ");
-        String n = sc.nextLine();
-        System.out.println("Name: " + n);
-    }
-}
